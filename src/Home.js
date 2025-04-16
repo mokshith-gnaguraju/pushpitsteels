54a5992b9fe35a5b7adeff21d9c608a10a3fa4c3
mokshith-gnaguraju <dhanamjeevig91@gmail.com>
2025-04-16 13:00:40 +0530

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div style={{textAlign:"center",backgroundColor:"yellow"}}>
+
+        <h1> Welcome to The New World</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/navbar/Zoom.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Zoom() {
+  return (
+
+    <div>
+      <h1> Power Star Pawan Kalyan</h1>
+     
+     <form>
+     <h3>
+      Pawan Kalyan enjoys one of the biggest fan bases in South India.He is loved for his simplicity,
+       mass dialogues, and fearless attitude in both cinema and politics.
+       Fans often call him "The Power Star" for his dynamic screen presence and leadership.
+       </h3>
+       </form>
+     
+       </div>
+
+  );
+}
+
+export default Zoom
